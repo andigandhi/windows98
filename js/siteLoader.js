e2694f8f7d24c75ff72b9092633a67d6078732ce
Andreas Grasser <andigrasser13@gmail.com>
2021-03-09 16:22:42 +0100

    Added all the JS for the taskbar

--- js/siteLoader.js
+++ js/siteLoader.js
@@ -34,14 +34,16 @@ function addWindow(title, innerHtml, w, h, left, top) {
 	
 	var titleBarIcon = document.createElement('div');
 	titleBarIcon.setAttribute('class', 'title-bar-controls');
-	titleBarIcon.innerHTML = "<button aria-label=\"Close\" onClick=\"hideWindow("+id+")\"></button>";
+	titleBarIcon.innerHTML = "<button aria-label=\"Minimize\" onClick=\"toggleWindow("+id+")\"></button>";
+	//titleBarIcon.innerHTML += "<button aria-label=\"Maximize\" onClick=\"maximizeWindow("+id+")\"></button>";
+	titleBarIcon.innerHTML += "<button aria-label=\"Close\" onClick=\"removeWindow("+id+")\"></button>";
 	titleBar.appendChild(titleBarIcon);
 	
 	win.appendChild(titleBar);
 	
 	var winInnen = document.createElement('div');
 	winInnen.setAttribute('class', 'window-body');
-	winInnen.style = "position: relative; height: 450px; overflow: hidden;";
+	winInnen.style = "position: relative; height: "+h+"px; overflow: hidden;";
 	winInnen.innerHTML = innerHtml;
 	
 	win.appendChild(winInnen);
@@ -49,10 +51,36 @@ function addWindow(title, innerHtml, w, h, left, top) {
 	addMoveListeners(win, titleBar);
 	
 	document.body.appendChild(win);
+	
+	
+	var task = document.createElement('button');
+	task.setAttribute('id', id+'t');
+	task.setAttribute('class', 'taskElement active');
+	task.setAttribute('onClick', 'toggleWindow('+id+')');
+	task.innerHTML = "<b>"+title+"</b>";
+	document.getElementById("taskbar").appendChild(task);
+}
+
+function maximizeWindow(id) {
+	var win = document.getElementById(id);
+	
+	w = "100%";
+	h = "100%";
+	
+	if (win.style.width == "100%") {
+		w = "816px";
+		h = "480px"
+	} else {
+		win.style.top = "0";
+		win.style.left = "0";
+	}
+	
+	win.style.width = w;
+	win.style.height = h;
 }
 
 // Creates the inner html for a Window and calls addWindow()
-function fillWindow(no) {
+function fillWindow(no, w, h) {
 	let title = "";
 	let link = "";
 	let innerHTML = "";
@@ -62,31 +90,49 @@ function fillWindow(no) {
 	} else {
 		title = siteLinks[no][0];
 		link = siteLinks[no][1];
-	}
-	
-	if (link.startsWith("http")) {
-		innerHTML='<iframe width="800" height="450" type="text/html" src="'+link+'" frameborder="0" allowfullscreen onmouseover = "mouseMove(\'event\')"></iframe>';
-	} else {
-		innerHTML='<object type="text/html" data="'+link+'" width="800px" height="450px" style="overflow-right: hidden;" onmouseover = "mouseMove(\'event\')"></object>';
-		//innerHTML='<iframe width="800" height="450" type="text/html" src="'+link+'" frameborder="0" allowfullscreen onmouseover = "mouseMove(\'event\')"></iframe>';
+		
+		w = siteLinks[no][2];
+		h = siteLinks[no][3];
 	}
 	
 	var left = Math.floor((Math.random() * 20) + 30);
 	var top = Math.floor((Math.random() * 15) + 1);
 	
-	addWindow(title, innerHTML, 816, 480, left, top);
-}
-
-// Creates the inner html for a popup window (smaller than a regular one) and calls addWindow()
-function addPopup() {
-	var left = Math.floor((Math.random() * 80) + 0);
-	var top = Math.floor((Math.random() * 20) + 0);
-	addWindow("Werbung", "<img alt='' src='img/corona-sticker-werbung.jpg' width=100% onclick='alert(\"Schreib mir einfach auf Instagram :)\")' style='cursor: pointer'>", 400, 300, left, top);
+	if (typeof(w) === 'undefined') {
+		w = 816;
+		h = 480;
+	}
+	
+	if (link.startsWith("http")) {
+		innerHTML='<iframe width="'+(w-16)+'px" height="'+(h-30)+'px" type="text/html" src="'+link+'" frameborder="0" allowfullscreen onmouseover = "mouseMove(\'event\')"></iframe>';
+	} else {
+		innerHTML='<object type="text/html" data="'+link+'" width="'+(w-16)+'px" height="'+(h-30)+'px" style="overflow-right: hidden;" onmouseover = "mouseMove(\'event\')"></object>';
+		//innerHTML='<iframe width="800" height="450" type="text/html" src="'+link+'" frameborder="0" allowfullscreen onmouseover = "mouseMove(\'event\')"></iframe>';
+	}
+	
+	addWindow(title, innerHTML, w, h, left, top);
 }
 
 // Removes a window with a specific ID
-function hideWindow(id) {
+function removeWindow(id) {
 	document.body.removeChild(document.getElementById(id));
+	document.getElementById("taskbar").removeChild(document.getElementById(id+'t'));
+}
+
+// Removes a window with a specific ID
+function toggleWindow(id) {
+	var win = document.getElementById(id);
+	var btn = document.getElementById(id+'t');
+	
+	if (win.style.visibility == "hidden") {
+		win.style.visibility = "";
+		btn.classList.add("active");
+		inForground(win);
+	} else {
+		win.style.visibility = "hidden";
+		btn.classList.remove("active");
+	}
+	//document.getElementById("taskbar").removeChild(document.getElementById(id+'t'));
 }
 
 
@@ -116,12 +162,25 @@ function build_menu() {
 	}
 }
 
+// Toggles the visibility of the menu
+function toggleMenu() {
+	var men = document.getElementById("menuWind");
+	var btn = document.getElementById("taskMenBtn");
+	
+	if (men.style.visibility == "hidden") {
+		men.style.visibility = "";
+		btn.classList.add("active");
+	} else {
+		men.style.visibility = "hidden";
+		btn.classList.remove("active");
+	}
+}
+
 // Creates a desktop icon
-function createIcon(name, link) {
+function createIcon(name, link, w, h) {
 	var ico = document.createElement('div');
 	ico.setAttribute('class', 'icon');
-	ico.style.overflow = "hidden";
-	ico.innerHTML = '<img alt="" src="img/ico/'+name+'.png" width="100%" style="cursor: pointer;" onClick="fillWindow([\''+name+'\', \''+link+'\']);">';
+	ico.innerHTML = '<img alt="" src="img/ico/'+name+'.png" width="100%" style="cursor: pointer;" onClick="fillWindow([\''+name+'\', \''+link+'\'], '+w+', '+h+');">';
 	ico.innerHTML += name;
 	
 	addMoveListeners(ico, ico);
@@ -132,7 +191,7 @@ function createIcon(name, link) {
 // Creates all the icons of the array icon[] by calling createIcon()
 function createIcons() {
 	for (var i = 0; i < icons.length; i++) {
-		createIcon(icons[i][0], icons[i][1]);
+		createIcon(icons[i][0], icons[i][1], icons[i][2], icons[i][3]);
 	}
 }
 
